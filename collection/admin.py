51d9f1a495a5f3f3c3206bef698eb39cb3408d14
kendrarose <kendrarose@grdylan.net>
2017-06-06 11:51:15 -0700

    "Chapter 6"

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from collection.models import Project # import model Project(thing in book)Chapter 6
+
+#autimatic slug creation.  Chapter 6.
+class ProjectAdmin(admin.ModelAdmin): #Changed ThingAdmin to ProjectAdmin
+    model = Project #changed Thing to Project
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Project, ProjectAdmin) # register model.  Chapter 6  Added ProjectAdmin(ThingAdmin in the book)
--- collection/models.py
+++ collection/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model): #I am making a collection of projects, rather than a collection of things. Added Chapter
+    name = models.CharField(max_length=255) #define name of project(thing in book).  Chapter 6
+    description = models.TextField() #define description of project(thing in book).  Chapter 6
+    slug = models.SlugField(unique=True) #define slug of project(thing in book). Chapter 6
